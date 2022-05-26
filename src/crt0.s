17c76cfc79c22f67b6d406689ed718a9a11e8543
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-05-26 15:51:04 +0700

    Save boot device address

--- src/crt0.s
+++ src/crt0.s
@@ -1,10 +1,14 @@
-.export __STARTUP__
+.export __STARTUP__, _boot_device_addr
 .import _main
-.importzp sp
+.importzp sp, ptr1, ptr2
 
 .segment "STARTUP"
 
 .proc __STARTUP__
+    ; Beep to let the user know the kernel loaded successfully
+    lda #$07
+    sta $2ff
+
     ; Yank the interrupt vectors from the BIOS. We don't want the BIOS to process them.
     lda #<int_handler
     ldx #>int_handler
@@ -15,23 +19,41 @@
     sta $fffa ; NMI
     stx $fffb
 
+    ; Save the address of the boot device.
+    lda #$00 ; Source address
+    ldx #$20
+    sta ptr1
+    stx ptr1+1
+
+    lda #<_boot_device_addr ; Destination address
+    ldx #>_boot_device_addr
+    sta ptr2
+    stx ptr2+1
+
+    ldy #0
+
+loop:
+    lda (ptr1),y
+    sta (ptr2),y
+    iny
+    cpy #16
+    bne loop
+
     ; Set up C stack
     lda #<stack_end
     ldx #>stack_end
     sta sp
     stx sp+1
 
-    ; Beep before we start
-    lda #$07
-    sta $2ff
-
     jsr _main
 hang: jmp hang
+.endproc
+
+.proc int_handler
+    rti ; Do nothing
 .endproc
 
 stack_begin: .res 128
 stack_end:
 
-.proc int_handler
-    rti ; Do nothing
-.endproc
+_boot_device_addr: .res 16
