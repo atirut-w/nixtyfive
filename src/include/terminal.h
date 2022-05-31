954f772654da3b9315d96acc51acbb6ae3d45b9b
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-05-31 23:08:32 +0700

    Fix conflicting types
    Somehow forgot to test before committing

--- src/include/terminal.h
+++ src/include/terminal.h
@@ -2,7 +2,7 @@
 #define TERMINAL_H_INCLUDED
 
 void print(const char *str); // Print a string to the terminal
-void printbyte(char byte); // Print a byte to the terminal
-void printshort(short word); // Print a short to the terminal
+void printbyte(const char byte); // Print a byte to the terminal
+void printshort(const short word); // Print a short to the terminal
 
 #endif
